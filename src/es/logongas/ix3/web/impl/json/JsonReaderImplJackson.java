9636b193ad76dc5b29f221343a54624a7c3e4463
Lorenzo <logongas@gmail.com>
2013-04-09 10:46:11 +0200

    Primera versión de ix3web. Contiene los controladores: REST de las entidades La autenticación de usuarios basándose en ix3security

---
+++ src/es/logongas/ix3/web/impl/json/JsonReaderImplJackson.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2013 Lorenzo González.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package es.logongas.ix3.web.impl.json;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import es.logongas.ix3.web.services.json.JsonReader;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Lorenzo González
+ */
+public class JsonReaderImplJackson implements JsonReader {
+    private Class clazz;
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    public JsonReaderImplJackson(Class clazz) {
+        this.clazz = clazz;
+    }
+
+    @Override
+    public Object fromJson(String json) {
+        try {
+            return objectMapper.readValue(json, clazz);
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+}
