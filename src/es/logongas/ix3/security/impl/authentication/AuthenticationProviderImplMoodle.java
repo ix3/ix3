dfa932edf2494d3c7f4355acc4469797be04d263
Lorenzo <logongas@gmail.com>
2013-06-06 10:49:26 +0200

    Añadir la validación de moodle

---
+++ src/es/logongas/ix3/security/impl/authentication/AuthenticationProviderImplMoodle.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright 2013 Lorenzo González.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package es.logongas.ix3.security.impl.authentication;
+
+import es.logongas.ix3.security.services.authentication.AuthenticationProvider;
+import es.logongas.ix3.security.services.authentication.Credential;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+/**
+ * Autenticar a un usuario mediante el usuario y contraseña de moodle
+ *
+ * @author Lorenzo González
+ */
+public class AuthenticationProviderImplMoodle implements AuthenticationProvider {
+
+    private String moodleLoginURL="http://www.fpmislata.com/moodle/login/index.php";
+
+    @Override
+    public boolean authenticate(Credential credential) {
+        try {
+            if ((credential instanceof CredentialImplLoginPassword) == false) {
+                return false;
+            }
+            CredentialImplLoginPassword credentialImplLoginPassword = (CredentialImplLoginPassword) credential;
+
+            DefaultHttpClient httpClientPost = new DefaultHttpClient();
+            HttpPost httpPost = new HttpPost(moodleLoginURL);
+            List<NameValuePair> nvps = new ArrayList<NameValuePair>();
+            nvps.add(new BasicNameValuePair("username", credentialImplLoginPassword.getLogin()));
+            nvps.add(new BasicNameValuePair("password", credentialImplLoginPassword.getPassword()));
+            httpPost.setEntity(new UrlEncodedFormEntity(nvps));
+            HttpResponse response1 = httpClientPost.execute(httpPost);
+            InputStream inputStream = response1.getEntity().getContent();
+            Document document = Jsoup.parse(inputStreamToString(inputStream));
+
+            Elements divElements = document.getElementsByClass("logininfo");
+            if (divElements.size() == 0) {
+                return false;
+            }
+            Element divElement = divElements.get(0);
+            Elements aElements = divElement.getElementsByTag("a");
+            if (aElements.size() == 0) {
+                return false;
+            }
+            Element aElement = aElements.get(aElements.size() - 1);
+            if (aElement.attr("href").indexOf("logout") < 0) {
+                return false;
+            }
+
+            DefaultHttpClient httpclient2 = new DefaultHttpClient();
+            HttpGet httpGet = new HttpGet(aElement.attr("href"));
+
+            httpclient2.execute(httpGet);
+
+            return true;
+
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    private String inputStreamToString(InputStream inputStream) {
+        try {
+            String line;
+            StringBuilder sb = new StringBuilder();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            while (null != (line = reader.readLine())) {
+                sb.append(line);
+                sb.append('\n');
+            }
+            return sb.toString();
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    /**
+     * @return the moodleLoginURL
+     */
+    public String getMoodleLoginURL() {
+        return moodleLoginURL;
+    }
+
+    /**
+     * @param moodleLoginURL the moodleLoginURL to set
+     */
+    public void setMoodleLoginURL(String moodleLoginURL) {
+        this.moodleLoginURL = moodleLoginURL;
+    }
+}
