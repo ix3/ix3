97ff8ae76f73f36dfa4b063b6e9550f3c56dd9cc
logongas <lorenzo.profesor@gmail.com>
2013-12-31 16:32:10 +0100

    Añadir las validaciones de Date y Time

---
+++ src/es/logongas/ix3/persistence/impl/constraints/TimeValidator.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2013 Lorenzo.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package es.logongas.ix3.persistence.impl.constraints;
+
+import es.logongas.ix3.persistence.services.constraints.Time;
+import java.util.GregorianCalendar;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+/**
+ * Valida que el Date esté en la época 0 , es decir el 1 de Enero de 1970
+ * Es decir que solo haya columnas de "Tiempo".
+ * Además comprueba que los milisegundos sean 0.Para de esa forma comparar mejor las fechas.
+ * @author Lorenzo
+ */
+public class TimeValidator implements ConstraintValidator<Time, java.util.Date> {
+
+    @Override
+    public void initialize(Time date) {
+    }
+
+    @Override
+    public boolean isValid(java.util.Date date, ConstraintValidatorContext cvc) {
+        GregorianCalendar gc=new GregorianCalendar();
+        gc.setTime(date);
+        
+        int year=gc.get(GregorianCalendar.YEAR);
+        int month=gc.get(GregorianCalendar.MONTH);
+        int day=gc.get(GregorianCalendar.DAY_OF_MONTH);
+        int mili=gc.get(GregorianCalendar.MILLISECOND);
+        
+        if ((year==1970) && (month==0) && (day==1) && (mili==0)) {
+            return true;
+        } else {
+            return false;
+        }
+        
+    }
+    
+}
---
+++ src/es/logongas/ix3/persistence/services/constraints/Date.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2013 Lorenzo.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package es.logongas.ix3.persistence.services.constraints;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+import es.logongas.ix3.persistence.impl.constraints.DateValidator;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.CONSTRUCTOR;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * Valida que un objeto Date no tenga nada mas que los datos de fecha y el tiempo esté a 00:00:00
+ * @author Lorenzo
+ */
+@Documented
+@Constraint(validatedBy = DateValidator.class)
+@Target({METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER})
+@Retention(RUNTIME)
+public @interface Date {
+
+    String message() default "{es.logongas.ix3.persistence.services.constraints.Date.message}";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+    /**
+     * Defines several {@code @Date} annotations on the same element.
+     */
+    @Target({METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER})
+    @Retention(RUNTIME)
+    @Documented
+    public @interface List {
+
+        Date[] value();
+    }
+}
