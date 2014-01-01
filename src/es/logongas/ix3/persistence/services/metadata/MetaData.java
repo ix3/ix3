d6dfec86b49e788cd7f3b141367c17386badc7a9
logongas <lorenzo.profesor@gmail.com>
2014-01-01 21:01:37 +0100

    Añadir comentarios

--- src/es/logongas/ix3/persistence/services/metadata/MetaData.java
+++ src/es/logongas/ix3/persistence/services/metadata/MetaData.java
@@ -83,22 +83,61 @@ public interface MetaData {
      */
     boolean isCollectionLazy();
 
+    /**
+     * El nombre de la propiedad.
+     * Sera null si hace rererencia a una clase en vez de a una propiedad de una clase.
+     * @return El nombre de la propiedad.
+     */
     String getPropertyName();
     
+    /**
+     * El label de la propiedad.Es decir un nombre para ser mostrado al usuario.
+     * Sera null si hace rererencia a una clase en vez de a una propiedad de una clase.
+     * @return El label de la propiedad.
+     */
     String getCaption();
 
+    /**
+     * Si esta propiedad es requerida
+     * Sera null si hace rererencia a una clase en vez de a una propiedad de una clase.
+     * @return Retorna <code>true</code> si la propiedad es requerida.
+     */
     boolean isRequired();
 
+    /**
+     * Para valores numéricos, el valor mínimo que tendrá.Por defecto vale Long.MIN_VALUE si no se indica ningún valor.
+     * @return Retorna el valro mínimo que puede tener un valor. 
+     */
     long getMinimum();
-
+    
+    /**
+     * Para valores numéricos, el valor máximo que tendrá.Por defecto vale Long.MAX_VALUE si no se indica ningún valor.
+     * @return Retorna el valor mínimo que puede tener un valor. 
+     */
     long getMaximum();
 
+    /**
+     * Para valores de tipo String, indica la longitud mínima del String.Por defecto vale 0 si no se indica ningún valor.
+     * @return La longitud mínima del String
+     */
     int getMinLength();
 
+    /**
+     * Para valores de tipo String, indica la longitud máxima del String.Por defecto vale Integer.MAX_VALUE si no se indica ningún valor.
+     * @return La longitud máxima del String
+     */
     int getMaxLength();
     
+    /**
+     * Para valores de tipo String, indica una expresión regular que debe cumplir el campo.Por defecto vale null si no se indica ningún valor.
+     * @return Una expresion regular
+     */
     String getPattern();
-
+    
+    /**
+     * Para valores de tipo String, indica un formato especifico que debe cumplir el campo.Por defecto vale null si no se indica ningún valor.
+     * @return El formato especifico
+     */
     Format getFormat();
     
 }
