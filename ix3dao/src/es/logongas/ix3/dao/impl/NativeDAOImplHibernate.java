703f17b2aa4820b5f617e292f436bd230cefd9ba
logongas <lorenzo.profesor@gmail.com>
2015-04-27 14:11:35 +0200

    Servicio REST de Echo para comprobar el servidor y la base de datos

---
+++ ix3dao/src/es/logongas/ix3/dao/impl/NativeDAOImplHibernate.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2015 logongas.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package es.logongas.ix3.dao.impl;
+
+import es.logongas.ix3.dao.NativeDAO;
+import java.util.List;
+import java.util.Map;
+import org.hibernate.SQLQuery;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * Lanza consultas nativas usando Hibernate
+ *
+ * @author logongas
+ */
+public class NativeDAOImplHibernate implements NativeDAO {
+
+    @Autowired
+    protected SessionFactory sessionFactory;
+
+    @Override
+    public List<Object> createNativeQuery(String query, List<Object> params) {
+        Session session = sessionFactory.getCurrentSession();
+
+        SQLQuery sqlQuery = session.createSQLQuery(query);
+
+        if (params != null) {
+            for (int i = 0; i < params.size(); i++) {
+                sqlQuery.setParameter(i, params.get(i));
+            }
+        }
+
+        return sqlQuery.list();
+    }
+
+    @Override
+    public List<Object> createNativeQuery(String query, Map<String, Object> params) {
+        Session session = sessionFactory.getCurrentSession();
+
+        SQLQuery sqlQuery = session.createSQLQuery(query);
+        
+        if (params != null) {
+            for (String paramName : params.keySet()) {
+                sqlQuery.setParameter(paramName, params.get(paramName));
+            }
+        }
+        return sqlQuery.list();
+    }
+
+}
---
+++ ix3web/src/es/logongas/ix3/web/controllers/EchoController.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright 2015 logongas.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package es.logongas.ix3.web.controllers;
+
+import es.logongas.ix3.core.BusinessException;
+import es.logongas.ix3.core.conversion.Conversion;
+import es.logongas.ix3.dao.NativeDAO;
+import es.logongas.ix3.dao.metadata.MetaData;
+import es.logongas.ix3.dao.metadata.MetaDataFactory;
+import es.logongas.ix3.service.CRUDService;
+import es.logongas.ix3.service.CRUDServiceFactory;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author logongas
+ */
+@Controller
+public class EchoController extends AbstractRESTController {
+
+    private static final Log log = LogFactory.getLog(EchoController.class);
+
+    @Autowired
+    private NativeDAO nativeDAO;
+
+    @RequestMapping(value = {"/$echo/{id}"}, method = RequestMethod.GET, produces = "application/json")
+    public void echo(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, final @PathVariable("id") int id) {
+        restMethod(httpServletRequest, httpServletResponse, null, new Command() {
+
+            @Override
+            public CommandResult run(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Map<String, Object> arguments) throws Exception, BusinessException {
+
+                List<Object> resultado = nativeDAO.createNativeQuery("select now() from dual", (List<Object>) null);
+                Date date = (Date) resultado.get(0);
+
+                EchoResult echoResult = new EchoResult(id, date);
+
+                return new CommandResult(EchoResult.class, echoResult);
+
+            }
+        });
+    }
+
+    public class EchoResult {
+
+        private int id;
+        private Date date;
+
+        public EchoResult(int id, Date date) {
+            this.id = id;
+            this.date = date;
+        }
+
+        /**
+         * @return the id
+         */
+        public int getId() {
+            return id;
+        }
+
+        /**
+         * @param id the id to set
+         */
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        /**
+         * @return the date
+         */
+        public Date getDate() {
+            return date;
+        }
+
+        /**
+         * @param date the date to set
+         */
+        public void setDate(Date date) {
+            this.date = date;
+        }
+
+        @Override
+        public String toString() {
+            return id + "-" + date.getTime();
+        }
+
+    }
+
+}
