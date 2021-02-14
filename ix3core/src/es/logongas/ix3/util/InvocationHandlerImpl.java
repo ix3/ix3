50073b500d2d4bc8d491c35c766eae66bcf13bc6
Lorenzo <lorenzo.profesor@gmail.com>
2021-02-14 16:29:11 +0100

    Añadir log de cuando no encuentra un método

--- ix3core/src/es/logongas/ix3/util/InvocationHandlerImpl.java
+++ ix3core/src/es/logongas/ix3/util/InvocationHandlerImpl.java
@@ -18,13 +18,17 @@ package es.logongas.ix3.util;
 import java.io.Serializable;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 /**
  * Esta clase es una ayuda para crear un Proxy de los interfaces deleganto la llamada a otro objeto.
  * @author Lorenzo González
  */
 public class InvocationHandlerImpl implements InvocationHandler {
-
+    
+    protected final Log log = LogFactory.getLog(getClass());
+    
     Object realObject;
 
     public InvocationHandlerImpl(Object realObject) {
@@ -49,6 +53,33 @@ public class InvocationHandlerImpl implements InvocationHandler {
             }
         }
 
+        //************* BEGIN:Quitar todo este código cuando se detecte el error *************************
+        //A veces pasa que no encuentra el método así que si llegamos hasta aqui es que no lo ha encontrado
+        //Así que vamos a añadir código para detectar que ha pasado.
+        log.warn("Nombre clase:"+realObjectClass.getName());
+        log.warn("método:" + method.toGenericString());        
+        for (Method realMethod : methods) {
+            if (realMethod.getName().equals(method.getName()) == true) {
+                if (equalParameterTypes(realMethod.getParameterTypes(),method.getParameterTypes())) {
+                    throw new RuntimeException("Se ha encontrado el método cuando antes no se había encontrado.");
+                } else {
+                   log.warn(realMethod.getName() + " no tiene los mismos parámetros que " + method.getName() + " ("  +realMethod.getParameterTypes().length + "," + method.getParameterTypes().length + ")"); 
+                   if (realMethod.getParameterTypes().length==method.getParameterTypes().length) {
+                        for(int i=0;i<realMethod.getParameterTypes().length;i++) {
+                            if (method.getParameterTypes()[i].isAssignableFrom(realMethod.getParameterTypes()[i])==false) {
+                                log.warn(method.getParameterTypes()[i].getName() + " no es del mismo tipo que " + realMethod.getParameterTypes()[i].getName()); 
+                            }
+                        }
+                   }
+                    
+                }
+            } else {
+                log.warn("nombre método " + realMethod.getName() + " es distinto a " + method.getName());
+            }
+        }
+        //************* END:Quitar todo este código cuando se detecte el error *************************
+        
+        
         throw new RuntimeException("No existe ese método:" + method.toGenericString());
     }
 
