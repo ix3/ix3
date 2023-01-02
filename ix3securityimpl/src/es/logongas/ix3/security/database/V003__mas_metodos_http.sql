24a0b131be5745640026cb70f62ddea0aef1060d
Lorenzo <lorenzo.profesor@gmail.com>
2023-01-02 14:56:08 +0100

    feat(#277):Permitir otros métodos HTTP

---
+++ ix3securityimpl/src/es/logongas/ix3/security/database/V003__mas_metodos_http.sql
@@ -0,0 +1,6 @@
+INSERT INTO `sec_permission` (`idPermission`, `name`, `description`, `idSecureResourceType`) VALUES
+	(6, 'HEAD', 'HEAD', 1),
+	(7, 'CONNECT', 'CONNECT', 1),
+	(8, 'OPTIONS', 'OPTIONS', 1),
+	(9, 'TRACE', 'TRACE', 1),
+	(10, 'PROPFIND', 'PROPFIND', 1);
